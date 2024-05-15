30b4e2de2b783d9628dcd020c8cc549274b74f05
Goutham Pai <31606207+gouthampai@users.noreply.github.com>
2024-05-15 18:33:06 -0500

    Initial integration with alpaca api (#2)
    * add and test alpaca go client package
    
    * add market client
    
    * compute SMA with vwap

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,21 +1,29 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
-	"net/http"
 	"os"
+
+	"github.com/alpacahq/alpaca-trade-api-go/v3/alpaca"
+	"github.com/alpacahq/alpaca-trade-api-go/v3/marketdata"
 )
 
 type config struct {
-	port int
-	env  string
+	port           int
+	env            string
+	alpacaEndpoint string
+	alpacaKey      string
+	alpacaSecret   string
 }
 
 type application struct {
-	config config
-	logger *log.Logger
+	config        config
+	logger        *log.Logger
+	accountClient *alpaca.Client
+	marketClient  *marketdata.Client
 }
 
 func main() {
@@ -23,16 +31,43 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "dev", "Environment (dev|stage|prod)")
+	flag.StringVar(&cfg.alpacaEndpoint, "alpaca-endpoint", os.Getenv("ALPACA_ENDPOINT"), "Alpaca Endpoint")
+	flag.StringVar(&cfg.alpacaKey, "alpaca-key", os.Getenv("ALPACA_KEY"), "Alpaca Key")
+	flag.StringVar(&cfg.alpacaSecret, "alpaca-secret", os.Getenv("ALPACA_SECRET"), "Alpaca Secret")
 	flag.Parse()
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	alpacaClient := alpaca.NewClient(alpaca.ClientOpts{
+		APIKey:    cfg.alpacaKey,
+		APISecret: cfg.alpacaSecret,
+		BaseURL:   cfg.alpacaEndpoint,
+	})
+
+	marketClient := marketdata.NewClient(marketdata.ClientOpts{
+		APIKey:    cfg.alpacaKey,
+		APISecret: cfg.alpacaSecret,
+	})
+
 	app := &application{
-		config: cfg,
-		logger: logger,
+		config:        cfg,
+		logger:        logger,
+		accountClient: alpacaClient,
+		marketClient:  marketClient,
 	}
 
-	router := app.route()
-	addr := fmt.Sprintf(":%d", app.config.port)
-	logger.Printf("starting %s server on %s", cfg.env, addr)
-	logger.Fatal(http.ListenAndServe(addr, router))
+	averages := app.CalculateMovingAverages("NVDA")
+	fmt.Printf("Last %v days of data\n", len(averages))
+	for i := 0; i < len(averages); i++ {
+		fmt.Printf("Date: %v\n50 day average: %v\n200 day average: %v\n\n", averages[i].dayOfYear, averages[i].fiftyDayAverage, averages[i].twoHundredDayAverage)
+	}
+}
+
+func (app *application) prettyPrint(v any) {
+	output, err := json.MarshalIndent(v, "", "\t")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf(string(output))
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module github.com/gouthampai/trading-strats
 
 go 1.22.1
 
-require github.com/julienschmidt/httprouter v1.3.0 // indirect
+require (
+	cloud.google.com/go v0.112.2 // indirect
+	github.com/alpacahq/alpaca-trade-api-go/v3 v3.4.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/shopspring/decimal v1.4.0 // indirect
+)
